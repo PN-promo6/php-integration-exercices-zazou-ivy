0057556502a811a6f47955d103f2e5c2a318083b
lea zaoui <lea.myriam.zaoui@gmail.com>
2020-04-14 14:55:30 +0200

    First commit

---
+++ exo1/script.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Exo01</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="script.css">
+</head>
+
+<body>
+    <div id="scroll-animate">
+        <div id="scroll-animate-main">
+            <div class="wrapper-parallax">
+                <header>
+                    <h1>Header</h1>
+                </header>
+
+                <section class="content">
+                    <h1>Content</h1>
+                </section>
+
+                <footer>
+                    <h1>Footer</h1>
+                </footer>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
